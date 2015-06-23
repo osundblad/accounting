138fa9decf70199949db31a9c04665561eecfe28
olle <olle@eris.se>
2015-06-23 23:49:09 +0200

    almost working (lacks hello rest service)

--- services/src/main/java/se/eris/accounting/Application.java
+++ services/src/main/java/se/eris/accounting/Application.java
@@ -3,13 +3,14 @@ package se.eris.accounting;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-//@Import(value = {ConfigInMemoryH2.class})
 public class Application {
 
     public static void main(@NotNull final String[] args) throws Exception {
