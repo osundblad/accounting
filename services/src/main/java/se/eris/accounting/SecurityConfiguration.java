e9a5a44d41331f03c2d79434657739c4fa01c401
olle <olle@eris.se>
2015-06-24 22:39:14 +0200

    csrf security

--- services/src/main/java/se/eris/accounting/SecurityConfiguration.java
+++ services/src/main/java/se/eris/accounting/SecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
@@ -13,8 +16,16 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .and()
-            .authorizeRequests()
-                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-                .anyRequest().authenticated();
+                  .authorizeRequests()
+                  .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+                  .authenticated()
+                .and()
+                  .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+                  .csrf().csrfTokenRepository(csrfTokenRepository());
     }
-}
+
+    private CsrfTokenRepository csrfTokenRepository() {
+      HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+      repository.setHeaderName("X-XSRF-TOKEN");
+      return repository;
+    }}
