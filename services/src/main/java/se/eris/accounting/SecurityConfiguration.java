cc08143956bbaed871398fa6e10214aba14ee64b
olle <olle@eris.se>
2015-06-24 21:51:11 +0200

    basic auth login page works

---
+++ services/src/main/java/se/eris/accounting/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package se.eris.accounting;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+            .authorizeRequests()
+                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                .anyRequest().authenticated();
+    }
+}
---
+++ services/src/main/java/se/eris/accounting/web/rest/AuthenticationResource.java
@@ -0,0 +1,18 @@
+package se.eris.accounting.web.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationResource {
+
+    @RequestMapping("")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+}
