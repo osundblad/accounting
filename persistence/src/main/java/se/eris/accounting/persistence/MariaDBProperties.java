ffd3d63914438ef909f841eea5f1ee67f303f847
olle <olle@eris.se>
2015-12-28 17:53:16 +0100

    added mariadb properties

---
+++ persistence/src/main/java/se/eris/accounting/persistence/MariaDBProperties.java
@@ -0,0 +1,63 @@
+package se.eris.accounting.persistence;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.NotNull;
+
+@ConfigurationProperties(prefix="mariadb")
+public class MariaDBProperties {
+
+    @NotNull
+    private Datasource datasource;
+
+    public Datasource getDatasource() {
+        return datasource;
+    }
+
+    public void setDatasource(final Datasource datasource) {
+        this.datasource = datasource;
+    }
+
+    private static class Datasource {
+
+        private String url;
+        private Class driverClassName;
+        private String userName;
+        private String password;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(final String url) {
+            this.url = url;
+        }
+
+
+        public Class getDriverClassName() {
+            return driverClassName;
+        }
+
+        public void setDriverClassName(final Class driverClassName) {
+            this.driverClassName = driverClassName;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(final String userName) {
+            this.userName = userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(final String password) {
+            this.password = password;
+        }
+
+    }
+
+}
